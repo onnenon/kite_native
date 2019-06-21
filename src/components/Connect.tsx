69aba466ac43f430241e865f565d11f178dd0a99
Stephen Onnen <stephen.onnen@gmail.com>
2019-06-21 15:42:51 -0400

    Added state to connect component.

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -2,12 +2,25 @@ import React, { Component } from "react";
 import { Text, View, TextInput, Button } from "react-native";
 
 export default class Connect extends Component {
+  state = {
+    host: "Hostname",
+    port: "Port"
+  };
+
   render() {
     return (
       <View>
         <Text>Enter Server Hostname or IP</Text>
-        <TextInput value="Hostname" />
-        <TextInput value="Port" />
+        <TextInput
+          value={this.state.host}
+          onChangeText={host => this.setState({ host })}
+          clearTextOnFocus={true}
+        />
+        <TextInput
+          value={this.state.port}
+          onChangeText={port => this.setState({ port })}
+          clearTextOnFocus={true}
+        />
         <Button onPress={() => this.forceUpdate} title="Connect" />
       </View>
     );
