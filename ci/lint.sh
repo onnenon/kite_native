e1bc89e67f7bb6a044ecd4905fdec868ae06fb47
Stephen Onnen <stephen.onnen@gmail.com>
2019-07-30 22:45:09 -0500

    Finished lint script.

--- ci/lint.sh
+++ ci/lint.sh
@@ -10,7 +10,7 @@ curl "$GITHUB_API/repos/$DRONE_REPO/statuses/$DRONE_COMMIT?access_token=$GITHUB_
   -s \
   -H "Content-Type: application/json" \
   -X POST \
-  -d "{\"state\": \"pending\", \"description\": \"The TSLint style checking is in progress\", \"target_url\": \"$DRONE_REMOTE_URL\", \"context\": \"$CI_CONTEXT\"}" \
+  -d "{\"state\": \"pending\", \"description\": \"The TSLint style checking is in progress\", \"target_url\": \"$DRONE_BUILD_LINK\", \"context\": \"$CI_CONTEXT\"}" \
   > /dev/null
 
 # Run TSLint
@@ -35,5 +35,6 @@ curl "$GITHUB_API/repos/$DRONE_REPO/statuses/$DRONE_COMMIT?access_token=$GITHUB_
   -H "Content-Type: application/json" \
   -X POST \
   -d "{\"state\": \"$STATUS\", \"description\": \"$DESCRIPTION\", \"target_url\": \"$DRONE_BUILD_LINK \", \"context\": \"$CI_CONTEXT\"}" \
+  > /dev/null
 
 exit 0 # don't make the build fail, just notify GitHub
