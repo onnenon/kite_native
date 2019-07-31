fe13c7c2559c2c1caa56421095b795ec9fe5aeff
Stephen Onnen <stephen.onnen@gmail.com>
2019-07-30 22:11:02 -0500

    Added script

---
+++ ci/lint.sh
@@ -0,0 +1,39 @@
+/drone-eslint.sh
+#! /bin/bash
+
+GITHUB_API=https://api.github.com
+CI_CONTEXT="stylechecker/eslint"
+
+# Notify GitHub stylechecking has started
+echo 'Starting ESLint...'
+curl "$GITHUB_API/repos/$DRONE_REPO/statuses/$DRONE_COMMIT?access_token=$GITHUB_TOKEN" \
+  -s \
+  -H "Content-Type: application/json" \
+  -X POST \
+  -d "{\"state\": \"pending\", \"description\": \"The ESLint style checking is in progress\", \"target_url\": \"$DRONE_REMOTE_URL\", \"context\": \"$CI_CONTEXT\"}" \
+  > /dev/null
+
+# Run ESLint
+yarn lint
+EXIT_CODE=$?
+
+# Notify GitHub according to ESLint exit code
+if [[ $EXIT_CODE == 0 ]]
+then
+  export STATUS="success"
+  export DESCRIPTION="No style issues"
+  echo $DESCRIPTION
+else
+  export STATUS="failure"
+  export DESCRIPTION="ESLint found style issues"
+  echo $DESCRIPTION
+fi
+
+curl "$GITHUB_API/repos/$CI_REPO/statuses/$CI_COMMIT?access_token=$GITHUB_TOKEN" \
+  -s \
+  -H "Content-Type: application/json" \
+  -X POST \
+  -d "{\"state\": \"$STATUS\", \"description\": \"$DESCRIPTION\", \"target_url\": \"$CI_BUILD_URL\", \"context\": \"$CI_CONTEXT\"}" \
+  > /dev/null
+
+exit 0 # don't make the build fail, just notify GitHub
